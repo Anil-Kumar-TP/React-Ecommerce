2aed2619c2d6afa4067f6d4ec450cfccecb01b6a
Anil <anil6996f@gmail.com>
2024-04-30 21:23:52 +0530

    Configured routes via react-router-dom and working on ProductsCard

--- src/components/ProductsCard.js
+++ src/components/ProductsCard.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
-const ProductsCard = () => {
+const ProductsCard = ({ product }) => {
   return (
-    <div>ProductsCard</div>
+    <div className='group'>
+        <div className='w-full h-96 cursor-pointer overflow-hidden'>
+            <img src={product.image} alt="Product" className='w-full h-full object-cover group-hover:scale-110 duration-500'/>  
+        </div>  
+          <div className='w-full border-[1px] px-2 py-4'>
+            <div className='flex justify-between items-center'>
+                <div>
+                    <h2 className='font-titleFont text-base font-bold'>{product.title}</h2>   
+                </div>   
+                <div className='flex gap-2'>
+                    <p className='line-through text-gray-500'>${product.oldPrice}</p>
+                    <p className='font-semibold'>${product.price}</p>
+                </div>
+            </div>
+        </div> 
+    </div>
   )
 }
 
